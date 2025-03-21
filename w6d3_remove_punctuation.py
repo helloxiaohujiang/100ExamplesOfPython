0327ded0ac677ced6f14d2aef6ef91606512820e
helloxiaohujiang <abeljiang2@gmail.com>
2025-03-21 10:10:49 +0800

    w6 Day6 completed.

--- w6d3_remove_punctuation.py
+++ w6d3_remove_punctuation.py
@@ -1,13 +1,11 @@
 # Remove Punctuation from a File
+
+import string
+
 with open("my-file.txt", "r", encoding='utf-8') as f:
-    for line in f:
-        count_line += 1
-        
-        words = line.split()
-        count_word += len(words)
-            
-        count_character += len(line)
-        
-print(f"The file has {count_line} lines.")
-print(f"The file has {count_word} words.")
-print(f"The file has {count_character} characters.")
+    text = f.read()
+    
+clean_text = text.translate(str.maketrans("","",string.punctuation))
+
+with open("my-clean-file.txt", "w", encoding='utf-8') as f:
+    f.write(clean_text)
